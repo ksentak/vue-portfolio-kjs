bde2504008f340a6830d240b984aa308bd7017db
KSentak <k.sentak@outlook.com>
2020-10-08 23:56:05 -0400

    fix back-end errors

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('expresss');
-const app = express;
+const express = require('express');
+const app = express();
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 const nodemailer = require('./routes/nodemailer');
-const port = process.env.PORT || 3000;
-require('dotenv').config;
+require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -15,18 +15,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', nodemailer);
 
 // Production variable
-const liveSite = 1;
+// const liveSite = 1;
 
-// Heroku dev testing
+// Heroku production
 if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production build
-	app.use(express.static('client/dist'));
+	// Express will serve up production assets
+	app.use(express.static('client/build'));
 
 	// Express serves up index.html file if it doesn't recognize route
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 	});
 }
+
+// Live production
+// if (liveSite == 1) {
+// 	// Express will serve up production assets
+// 	app.use(express.static('client/build'));
+
+// 	// Express serves up index.html file if it doesn't recognize route
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+// 	});
+// }
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
