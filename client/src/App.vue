9778e71b800b0dc41c4dffc84352c13f3e84066a
Keaton Sentak <k.sentak@outlook.com>
2020-12-19 09:38:07 -0500

    set up vue routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,10 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/portfolio">Portfolio</router-link>
+    <router-link to="/contact">Contact</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Portfolio from '../views/Portfolio.vue';
+import Contact from '../views/Contact.vue';
 
 const routes = [
   {
@@ -10,16 +13,23 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: Portfolio
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
   }
-]
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
-})
+});
 
-export default router
+export default router;
